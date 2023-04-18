d49fd24ab645f18e36f71151b1a91e53f8f27d7e
Teppei <kcsoita.20210193@gmail.com>
2023-04-19 00:11:55 +0900

    change var names to snake_case

--- done.pl
+++ done.pl
@@ -4,22 +4,22 @@ use strict;
 use warnings;
 
 my $memo = "path of memo file"; # メモファイル
-my $doneLog = "path of DONE"; # DONEを保存するファイル
+my $done_log = "path of DONE"; # DONEを保存するファイル
 
-my @afterMemo;
+my @after_memo;
 my $flag = 0;
 
-unless (-f $doneLog) {
-    open DONE, ">", $doneLog
-        or die "cannot open $doneLog because $!\n";
+unless (-f $done_log) {
+    open DONE, ">", $done_log
+        or die "cannot open $done_log because $!\n";
     print DONE "*DONE logfile\n";
     close DONE;
 }
 
 open MEMO, "<", $memo
     or die "cannot open $memo because $!\n";
-open DONE, ">>", $doneLog
-    or die "cannot open $doneLog because $!\n";
+open DONE, ">>", $done_log
+    or die "cannot open $done_log because $!\n";
 
 for my $done (<MEMO>) {
     if ($done =~ /^\*\*\sDONE/) {
@@ -27,13 +27,13 @@ for my $done (<MEMO>) {
         $flag = 1;
     } elsif ($flag) {
         if ($done =~ /^\*+\s(?!DONE)/) {
-            push @afterMemo, $done;
+            push @after_memo, $done;
             $flag = 0;
         } else {
             print DONE $done;
         }
     } else {
-        push @afterMemo, $done;
+        push @after_memo, $done;
     }
 }
 close MEMO;
@@ -42,6 +42,6 @@ close DONE;
 open MEMO, ">", $memo
     or die "cannot open $memo because $!\n";
 
-print MEMO @afterMemo;
+print MEMO @after_memo;
 
 close MEMO;
